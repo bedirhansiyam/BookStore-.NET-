9eb7842f5bf29534ff366cf3a729fe08252e8a0a
Bedirhan <bedirhansiyam@gmail.com>
2023-03-27 02:29:17 +0400

    DataGenerator updated

--- WebApi/DBOperations/DataGenerator.cs
+++ WebApi/DBOperations/DataGenerator.cs
@@ -12,33 +12,35 @@ public class DataGenerator
             {
                 return;
             }
-
-            context.Books.AddRange(    
-                new Book
-                {
-                    //Id = 1,
-                    Title = "Lean Startup",
-                    GenreId = 1,
-                    PageCount = 200,
-                    PublishDate = new DateTime(2001,06,12)
-                },
-                new Book
-                {
-                    //Id = 2,
-                    Title = "Herland",
-                    GenreId = 2,
-                    PageCount = 250,
-                    PublishDate = new DateTime(2010,02,12)
-                },
-                new Book
-                {
-                    //Id = 3,
-                    Title = "Dune",
-                    GenreId = 2,
-                    PageCount = 540,
-                    PublishDate = new DateTime(2001,12,05)
-                }
+            else
+            {
+                context.Books.AddRange(    
+                    new Book
+                    {
+                        //Id = 1,
+                        Title = "Lean Startup",
+                        GenreId = 1,
+                        PageCount = 200,
+                        PublishDate = new DateTime(2001,06,12)
+                    },
+                    new Book
+                    {
+                        //Id = 2,
+                        Title = "Herland",
+                        GenreId = 2,
+                        PageCount = 250,
+                        PublishDate = new DateTime(2010,02,12)
+                    },
+                    new Book
+                    {
+                        //Id = 3,
+                        Title = "Dune",
+                        GenreId = 2,
+                        PageCount = 540,
+                        PublishDate = new DateTime(2001,12,05)
+                    }
             );
+            }          
             
             context.SaveChanges();
         }
